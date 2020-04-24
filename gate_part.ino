b5554acbcfa2f36c89789fede9fd580ee2ec9cd6
Shreevallabha A <60250051+asv0018@users.noreply.github.com>
2020-04-24 21:59:23 +0530

    Update gate_part.ino

--- gate_part.ino
+++ gate_part.ino
@@ -20,12 +20,12 @@ void turnGateOn(){
  for (int pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 90 degrees
     Serial.println(pos);
     gate.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(15);                       // waits 15ms for the servo to reach the position is 15*(90-0)=1350 ms
   }
 }
 void turnGateOff(){
   for (int pos = 90; pos >= 0; pos -= 1) { // goes from 90 degrees to 0 degrees
     gate.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(15);                     // waits 15ms for the servo to reach the position is 15*(90-0)=1350 ms
   }
 }
